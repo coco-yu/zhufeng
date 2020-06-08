6498e9c8f006a6473ce97beb8f72bbaf7be2220b
coco <coco@cocodeiMac.local>
2020-06-08 19:16:16 +0800

    update

--- event/event.js
+++ event/event.js
@@ -1,8 +1,22 @@
 function EventEmitter() {
-    this._events = {};
+    this._events = {}; // 给EventEmitter添加事件对象
 }
 
 EventEmitter.prototype.on = function (eventName, callback) {
-    if (!this._events) this._events = {};
-    (this._events[eventName] || t(his._events[eventName] = [])).push(callback);
+    if (!this._events) this._events = {}; // 给实例添加事件对象
+    (this._events[eventName] || (this._events[eventName] = [])).push(callback);
 }
+
+EventEmitter.prototype.emit = function (eventName, ...args) {
+    if (this._events && this._events[eventName]) {
+        this._events[eventName].forEach(event => event(...args));
+    }
+}
+
+EventEmitter.prototype.off = function (eventName, callback) {
+    if (this._events && this._events[eventName]) {
+        this._events[eventName] = this._events[eventName].filter(cb => cb != callback);
+    }
+}
+
+module.exports = EventEmitter;
