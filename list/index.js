538b8edb5b5fb8c3e7407c2af8be4350c4f26eeb
coco <coco@cocodeiMac.local>
2020-06-23 20:02:43 +0800

    链表

---
+++ list/index.js
@@ -0,0 +1,41 @@
+// 声明Node
+class Node {
+    constructor(element, next) {
+        this.element = element;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add(index, element) {
+        if (arguments.length === 0) {
+            // 只传入元素、没有索引 参数的配置
+            element = index;
+            index = this.size;
+        }
+
+        if (index < 0 && index > this.size) throw Error('越界');
+
+        if (index === 0) {
+            const head = this.head;
+            this.head = new Node(element, head);
+        } else {
+            const prevNode = this.getNode(index - 1);
+            prevNode.next = new Node(element, prevNode.next);
+        }
+        this.size++;
+    }
+
+    getNode(index) {
+        const current = this.head;
+        for (let i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+}
