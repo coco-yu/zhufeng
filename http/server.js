9bca76a69dab56fdae14a1080e9b0eaaec15cd21
coco-yu <359274291@qq.com>
2020-07-08 23:47:18 +0800

    create server

--- http/server.js
+++ http/server.js
@@ -1,18 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.write('hello');
-    // res.end();
+    // res.write('hello'); // 可写流、end之后不能再写write方法， end方法相当于write+end
+    console.log(req.method);
+    const { pathname, query } = url.parse(req.url, true);
+    console.log(pathname, query, '---');
+    console.log(req.httpVersion);
+    console.log(req.headers);
+    res.end(pathname + ' ' +JSON.stringify(query));
 });
 
 let port = 3000;
 
-server.listen(port, () => { 
+server.listen(port, () => {
     console.log('server start', port)
 });
 
 server.on('error', (err) => {
-    if(err.errno === 'EADDRINUSE') {
+    if (err.errno === 'EADDRINUSE') {
         server.listen(++port);
     }
 })
